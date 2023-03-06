d49c83020422cdf30e8224e033996f463a1a9f3a
ezequiel <ps3ezequiel@gmail.com>
2023-03-06 16:44:47 +0100

    mobile ok

---
+++ src/scripts/constants.js
@@ -0,0 +1,65 @@
+export const projects = [
+  {
+    img: 'fylo.jpg',
+    title: 'Fylo - Website',
+    skills: ['HTML', 'CSS', 'Tailwind'],
+    links: {
+      project: 'https://eze-alarcon.github.io/fylo/',
+      repo: 'https://github.com/Eze-Alarcon/fylo'
+    }
+  },
+  {
+    img: 'art-gallery.jpg',
+    title: 'Art Gallery - Website',
+    skills: ['HTML', 'CSS', 'Tailwind'],
+    links: {
+      project: 'https://art-gallery-home.netlify.app',
+      repo: 'https://github.com/Eze-Alarcon/art-gallery'
+    }
+  },
+  {
+    img: 'todo-app.jpg',
+    title: 'Todo App',
+    skills: ['React', 'JavaScript', 'CSS', 'Tailwind'],
+    links: {
+      project: 'https://eze-alarcon.github.io/TODO_APP/',
+      repo: 'https://github.com/Eze-Alarcon/TODO_APP'
+    }
+  },
+  {
+    img: 'tic-tac-toe.jpg',
+    title: 'Tic Tac Toe - Game',
+    skills: ['React', 'JavaScript', 'Tailwind'],
+    links: {
+      project: 'https://tic-tac-toe-home.netlify.app',
+      repo: 'https://github.com/Eze-Alarcon/tic-tac-toe'
+    }
+  },
+  {
+    img: 'planet-facts.jpg',
+    title: 'Fylo - Website',
+    skills: ['React', 'JavaScript', 'CSS'],
+    links: {
+      project: 'https://planet-facts-deploy.vercel.app/',
+      repo: 'https://github.com/Eze-Alarcon/planet-facts'
+    }
+  },
+  {
+    img: 'calculator.jpg',
+    title: 'Calculator - Website',
+    skills: ['JavaScript', 'HTML', 'CSS'],
+    links: {
+      project: 'https://eze-alarcon.github.io/Calculator-Frontend_Mentor/',
+      repo: 'https://github.com/Eze-Alarcon/Calculator-Frontend_Mentor'
+    }
+  },
+  {
+    img: 'photosnap.jpg',
+    title: 'Photosnap - Website',
+    skills: ['HTML', 'CSS', 'Tailwind'],
+    links: {
+      project: 'https://photosnap-home.netlify.app',
+      repo: 'https://github.com/Eze-Alarcon/photosnap-project'
+    }
+  }
+]
